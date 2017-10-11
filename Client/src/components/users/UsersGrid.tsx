522fc454ce0d1f86e248d98eb3e7d647afd0c44c
avgrankovskiy <grankovskiy@inbox.ru>
2017-10-11 09:49:07 +0300

    Добавлено использование async/await в коде запроса к серверу, добавлен индикатор загрузки на грид

--- Client/src/components/users/UsersGrid.tsx
+++ Client/src/components/users/UsersGrid.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Table, Header, Menu, Icon } from "semantic-ui-react";
+import { Button, Dimmer, Header, Icon, Loader, Menu, Segment, Table } from "semantic-ui-react";
 import {UserGridItem, UserService} from "../../services/UserService";
 import UsersGridRow from "./UsersGridRow";
 
@@ -8,80 +8,102 @@ export default class UsersGrid extends React.Component<{}, UsersGridState> {
         super(props);
 
         this.state = {
-            users: []
+            users: [],
+            isLoading: false
         };
     }
 
-    private getUsers(): void{
-        UserService.getUsersGrid()
-            .then((users) => {
-                this.setState({
-                    users
-                });
-            });
+    private async getUsers(): Promise<void>{
+        this.showLoading();
+
+        const users = await UserService.getUsersGrid();
+
+        this.setState({
+            users
+        });
+
+        this.hideLoading();
     }
 
     public componentDidMount(): void{
         this.getUsers();
     }
 
+    public showLoading(){
+        this.setState({
+            ...this.state, isLoading: true
+        });
+    }
+
+    public hideLoading(){
+        this.setState({
+            ...this.state, isLoading: false
+        });
+    }
+
     public render() {
         return (
-            <Table celled selectable striped definition>
-                <Table.Header fullWidth>
-                    <Table.Row>
-                        <Table.HeaderCell colSpan="6">
-                            <Button floated="right" primary content="Add user" icon="plus"/>
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Header fullWidth>
-                    <Table.Row>
-                        <Table.HeaderCell width={1}/>
-                        <Table.HeaderCell>
-                            Photo
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                            Name
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                            Login
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                            Email
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                            Activity
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {this.state.users.map((user) =>
-                        <UsersGridRow key={user.id} userItem={user}/>)}
-                </Table.Body>
-                <Table.Footer fullWidth>
-                    <Table.Row>
-                        <Table.HeaderCell colSpan="6">
-                            <Menu floated="right" pagination>
-                                <Menu.Item as="a" icon>
-                                    <Icon name="left chevron" />
-                                </Menu.Item>
-                                <Menu.Item as="a">1</Menu.Item>
-                                <Menu.Item as="a">2</Menu.Item>
-                                <Menu.Item as="a">3</Menu.Item>
-                                <Menu.Item as="a">4</Menu.Item>
-                                <Menu.Item as="a" icon>
-                                <Icon name="right chevron" />
-                                </Menu.Item>
-                            </Menu>
-                        </Table.HeaderCell>
-                    </Table.Row>
-            </Table.Footer>
-            </Table>
+            <Dimmer.Dimmable dimmed={this.state.isLoading}>
+                <Dimmer active={this.state.isLoading} inverted>
+                    <Loader>Loading</Loader>
+                </Dimmer>
+                <Table celled selectable striped definition>
+                    <Table.Header fullWidth>
+                        <Table.Row>
+                            <Table.HeaderCell colSpan="6">
+                                <Button floated="right" primary content="Add user" icon="plus"/>
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Header fullWidth>
+                        <Table.Row>
+                            <Table.HeaderCell width={1}/>
+                            <Table.HeaderCell>
+                                Photo
+                            </Table.HeaderCell>
+                            <Table.HeaderCell>
+                                Name
+                            </Table.HeaderCell>
+                            <Table.HeaderCell>
+                                Login
+                            </Table.HeaderCell>
+                            <Table.HeaderCell>
+                                Email
+                            </Table.HeaderCell>
+                            <Table.HeaderCell>
+                                Activity
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {this.state.users.map((user) =>
+                            <UsersGridRow key={user.id} userItem={user}/>)}
+                    </Table.Body>
+                    <Table.Footer fullWidth>
+                        <Table.Row>
+                            <Table.HeaderCell colSpan="6">
+                                <Menu floated="right" pagination>
+                                    <Menu.Item as="a" icon>
+                                        <Icon name="left chevron" />
+                                    </Menu.Item>
+                                    <Menu.Item as="a">1</Menu.Item>
+                                    <Menu.Item as="a">2</Menu.Item>
+                                    <Menu.Item as="a">3</Menu.Item>
+                                    <Menu.Item as="a">4</Menu.Item>
+                                    <Menu.Item as="a" icon>
+                                    <Icon name="right chevron" />
+                                    </Menu.Item>
+                                </Menu>
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
+            </Dimmer.Dimmable>
         );
     }
 }
 
 interface UsersGridState{
     users: UserGridItem[];
+    isLoading: boolean;
 }
