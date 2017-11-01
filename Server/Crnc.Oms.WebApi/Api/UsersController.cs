735bcd6e89b4463551c78f023b2a5ecb4cb48527
avgrankovskiy <grankovskiy@inbox.ru>
2017-11-01 09:57:49 +0300

    Добавлены rest методы для создания, изменения и удаления пользователей

--- Server/Crnc.Oms.WebApi/Api/UsersController.cs
+++ Server/Crnc.Oms.WebApi/Api/UsersController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Crnc.Oms.Domain.IRepositories;
 using CrncOmsWeb.DTO;
+using Crnc.Oms.Domain.Aggregates.Users;
+using UserEntity = Crnc.Oms.Domain.Aggregates.Users.User;
 
 namespace CrncOmsWeb.Api
 {
@@ -42,29 +44,50 @@ namespace CrncOmsWeb.Api
             }).ToList();           
         }
 
-        // GET api/values/5
+        // GET api/users/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public UserItemDto Get(int id)
         {
-            return "value";
+            var user = _userRepository.FindById(id);
+
+            return new UserItemDto()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                FullName = user.FullName,
+                Email = user.Email,
+                Password = user.PasswordHash,
+                Login = user.Login,
+                Phone = user.Phone,
+                Role = user.Role.Title,
+                PhotoBase64 = user.Photo.ContentBase64,
+                PhotoMimeType = user.Photo.MimeType,
+                IsActive = user.IsActive
+            };
         }
 
-        // POST api/values
+        // POST api/users
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]UserItemDto user)
         {
+            var entity = UserEntity.CreateNew(user.Login, user.Password, user.FirstName, user.LastName, user.Email, user.Phone);
+            _userRepository.Add(entity);
         }
 
-        // PUT api/values/5
+        // PUT api/users/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]UserItemDto user)
         {
+            var entity = UserEntity.CreateExisted(id,user.Login, user.Password, user.FirstName, user.LastName, user.Email, user.Phone);
+            _userRepository.Save(entity);
         }
 
-        // DELETE api/values/5
+        // DELETE api/users/5
         [HttpDelete("{id}")]
         public void Delete(int id)
-        {
+        {            
+            _userRepository.Delete(id);
         }
     }
 }
