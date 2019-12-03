19e83db11ac4e12b31fefd92d9add71e5f31275d
avgrankovskiy <grankovskiy@inbox.ru>
2019-12-03 17:08:56 +0300

    Выполнена настройка Docker-compose для Security проекта

--- src/Server/src/Crnc.Oms.Security/Crnc.Oms.Security.Infrastructure.DataAccess/MongoDbSettings.cs
+++ src/Server/src/Crnc.Oms.Security/Crnc.Oms.Security.Infrastructure.DataAccess/MongoDbSettings.cs
@@ -3,19 +3,6 @@ namespace Crnc.Oms.Security.Infrastructure.DataAccess
     public class MongoDbSettings
     {
         public string Database { get; set; }
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
-        
-        public string ConnectionString 
-        {
-            get 
-            {
-                if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
-                    return $@"mongodb://{Host}:{Port}";
-                return $@"mongodb://{User}:{Password}@{Host}:{Port}";
-            }
-        }
+        public string Server { get; set; }
     }
 }
