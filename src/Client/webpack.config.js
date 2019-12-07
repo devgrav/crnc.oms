869b6ab4005ed0d7ae076d4eb5bb6b966bbe7455
avgrankovskiy <grankovskiy@inbox.ru>
2019-12-07 16:51:32 +0300

    Added enviroment variables for frontend

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -2,54 +2,62 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require("path");
 
-module.exports = {
-	devtool: 'eval',
-	entry: [
-		'babel-polyfill',
-		'react-hot-loader/patch',
-		'./index.tsx'
-	],
-	output: {
-		filename: 'bundle.js',
-		publicPath: "/",
-		path: path.resolve(__dirname, "dist")
-	},
-	context: path.resolve(__dirname, 'src'),
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', 'json', '.jsx']
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.(ts|tsx)$/,
-				loader: ['react-hot-loader/webpack','babel-loader', 'ts-loader']
-			},
-			{
-				test: /\.css$/,
-				loader: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(gif|png|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
-				loader: [ 'url-loader']
-			}		
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin(),
-		new HtmlWebpackPlugin({
-			inject: true,
-			template: 'index.html'
-		})
-	],
-	devServer: {
-		hot: true,
-		inline: true,
-		host: "localhost",
-		port: "8090",
-		open :true,
-		historyApiFallback: true		
-		//Enable this if you want to never refresh (this allows hot-reloading app.tsx, but won't auto-refresh if you change index.tsx)
-		//hotOnly: true
+module.exports = env => {
+
+	return{
+		devtool: 'eval',
+		entry: [
+			'babel-polyfill',
+			'react-hot-loader/patch',
+			'./index.tsx'
+		],
+		output: {
+			filename: 'bundle.js',
+			publicPath: "/",
+			path: path.resolve(__dirname, "dist")
+		},
+		context: path.resolve(__dirname, 'src'),
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', 'json', '.jsx']
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.(ts|tsx)$/,
+					loader: ['react-hot-loader/webpack','babel-loader', 'ts-loader']
+				},
+				{
+					test: /\.css$/,
+					loader: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.(gif|png|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
+					loader: [ 'url-loader']
+				}		
+			]
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.NamedModulesPlugin(),
+			new HtmlWebpackPlugin({
+				inject: true,
+				template: 'index.html'
+			}),
+			new webpack.EnvironmentPlugin({
+				NODE_ENV: env.NODE_ENV || 'development',
+				REACT_APP_SECURITY_API_URL: env.REACT_APP_SECURITY_API_URL || 'http://localhost:64707/api',
+				REACT_APP_SALES_API_URL: env.REACT_APP_SALES_API_URL || 'http://localhost:64708/api'
+				})
+		],
+		devServer: {
+			hot: true,
+			inline: true,
+			host: "localhost",
+			port: "8090",
+			open :true,
+			historyApiFallback: true		
+			//Enable this if you want to never refresh (this allows hot-reloading app.tsx, but won't auto-refresh if you change index.tsx)
+			//hotOnly: true
+		}	
 	}
 };
