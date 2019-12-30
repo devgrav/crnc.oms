1b328ebf63e9fb50884c7d5b412f148947a2811c
Гранковский Андрей Васильевич <AGrankovsky@loko-domain.ru>
2019-12-30 19:22:44 +0300

    Добавлена форма создания заказа и переход в нее из списка, добавлен mobx, обновлены некоторые библиотеки, рефакторинг доменной модели

---
+++ src/Server/src/Crnc.Oms.Sales/Crnc.Oms.Sales.Application/Factories/OrderMapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Crnc.Oms.Sales.Application.Features.Orders.Dto.Input;
+using Crnc.Oms.Sales.Domain.Aggregates.Order;
+using Crnc.Oms.Sales.Domain.SeedWork;
+
+namespace Crnc.Oms.Sales.Application.Factories
+{
+    public static class OrderFactory
+    {
+        public static Order CreateNewOrder(CreateOrderInputDto dto, ICurrentDateTimeProvider currentDateTimeProvider)
+        {
+            var customer = new Customer(
+                new Title(dto.CustomerTitle, 
+                    new NameAbbreviation(dto.CustomerAbbreviation)), 
+                new ContactPerson(new FullName(
+                        dto.CustomerFirstName,
+                        dto.CustomerLastName,
+                        dto.CustomerMiddleName), 
+                    new Email(dto.CustomerContactPersonEmail),
+                    new Phone(dto.CustomerContactPersonPhone)));
+
+            var order = new Order(
+                Guid.NewGuid(), 
+                currentDateTimeProvider.GetNow(), 
+                dto.JobType, 
+                dto.JobDescription, 
+                customer);
+
+            return order;
+        }
+        
+        public static Order CreateExistedOrder(Order order, EditOrderInputDto dto)
+        {
+            var customer = new Customer(
+                new Title(dto.CustomerTitle, 
+                    new NameAbbreviation(dto.CustomerAbbreviation)), 
+                new ContactPerson(new FullName(
+                        dto.CustomerFirstName,
+                        dto.CustomerLastName,
+                        dto.CustomerMiddleName), 
+                    new Email(dto.CustomerContactPersonEmail),
+                    new Phone(dto.CustomerContactPersonPhone)));
+
+            order.Edit(dto.JobType, 
+                dto.JobDescription, 
+                dto.MaterialSource, 
+                dto.SignOffType, 
+                customer);
+
+            return order;
+        }
+        
+    }
+}
---
+++ src/Server/src/Crnc.Oms.Sales/Crnc.Oms.Sales.Application/Features/Orders/Dto/Output/TextValueOutputDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crnc.Oms.Security.WebApi.DTO
+{
+    public class TextValueDto
+    {
+        public Guid Value { get; set; }
+
+        public string Text { get; set; }
+    }
+}
