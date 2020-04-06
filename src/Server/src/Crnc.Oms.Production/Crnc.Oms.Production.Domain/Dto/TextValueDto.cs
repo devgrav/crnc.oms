4f76cca98635ca27bbbdecd1285599fe9f199e82
avgrankovskiy <grankovskiy@inbox.ru>
2020-04-07 02:22:37 +0300

    Добавлен микросервис производства, создание и изменение работы, изменение приоритета и закрытие работы

---
+++ src/Server/src/Crnc.Oms.Production/Crnc.Oms.Production.Domain/Dto/TextValueDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crnc.Oms.Sales.Application.Features.Orders.Dto.Output
+{
+    public class TextValueOutputDto<Tkey,TValue>
+    {
+        public Tkey Value { get; set; }
+
+        public TValue Text { get; set; }
+    }
+}
